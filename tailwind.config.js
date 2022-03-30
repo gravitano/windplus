0a2762898289e80abe4f5b7a4f2c9cca9ff52573
Warsono <warsono16694@gmail.com>
2022-03-30 13:54:14 +0700

    feat: alert

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+        secondary: colors.pink,
+        info: colors.sky,
+        warning: colors.amber,
+        error: colors.red,
+        success: colors.emerald,
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
