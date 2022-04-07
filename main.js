0534fbe74fef813453f63f21c1955c261d744e7f
Warsono <warsono16694@gmail.com>
2022-04-08 00:55:06 +0700

    fix: add space

--- main.js
+++ main.js
@@ -210,7 +210,7 @@ app.component('AppSidebar', {
 app.component('AppFooter', {
   template: `
     <footer class="container mx-auto text-sm text-gray-500 py-3 flex justify-center border-t">
-      Copyright &copy; 2022-Present &middot; <a href="https://github.com/gravitano">Warsono</a>
+      Copyright &copy; 2022-Present &middot;&nbsp; <a href="https://github.com/gravitano">Warsono</a>
     </footer>
     `
 })
