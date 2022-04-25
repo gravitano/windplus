83b954c779b7480157a7a7944b9e9a5308cdcf43
Warsono <warsono16694@gmail.com>
2022-04-25 13:14:42 +0700

    chore: add vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import { fileURLToPath, URL } from 'url';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  plugins: [vue()],
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
