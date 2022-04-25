cc06af134523497dd083507d3c0eed59a87d7599
Warsono <warsono16694@gmail.com>
2022-04-25 15:59:40 +0700

    fix: base layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import AppHeader from '../components/AppHeader.vue';
 import AppFooter from '../components/AppFooter.vue';
 import AppSidebar from '../components/AppSidebar.vue';
+import { useRoute } from 'vue-router';
 
 const props = defineProps({
-  hideSidebar: Boolean,
-  fluid: Boolean,
   menus: Array,
 });
 
@@ -19,6 +18,9 @@ const scrollTo = (menu) => {
   });
   activeHref.value = menu.href;
 };
+
+const route = useRoute();
+const isHomePage = computed(() => route.name === 'index');
 </script>
 
 <template>
@@ -26,26 +28,26 @@ const scrollTo = (menu) => {
 
   <div
     class="flex flex-col sm:flex-row gap-6 mx-auto"
-    :class="fluid ? '' : 'container py-4'"
+    :class="isHomePage ? '' : 'container py-4'"
   >
-    <app-sidebar v-model="sidebar" :hide="hideSidebar"></app-sidebar>
+    <app-sidebar v-model="sidebar" :hide="isHomePage"></app-sidebar>
 
     <main
       class="w-full flex gap-4"
-      :class="hideSidebar ? 'flex-col' : 'flex-col sm:flex-row px-4 sm:px-0'"
+      :class="isHomePage ? 'flex-col' : 'flex-col sm:flex-row px-4 sm:px-0'"
     >
       <div
         class="order-2 sm:order-2"
-        :class="hideSidebar ? '' : 'w-full sm:w-9/12'"
+        :class="isHomePage ? '' : 'w-full sm:w-9/12'"
       >
-        <div :class="hideSidebar ? '' : 'container mx-auto prose'">
+        <div :class="isHomePage ? '' : 'container mx-auto prose'">
           <router-view />
         </div>
       </div>
 
       <div
         class="sm:h-screen sm:sticky sm:top-20 relative order-1 sm:order-2"
-        :class="hideSidebar ? 'hidden' : 'w-full sm:w-3/12'"
+        :class="isHomePage ? 'hidden' : 'w-full sm:w-3/12'"
       >
         <template v-if="menus && menus.length">
           <button
